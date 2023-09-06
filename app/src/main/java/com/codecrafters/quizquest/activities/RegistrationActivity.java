f9e1fa1d2af95db700d383c2ac26d99656f1b37d
Avinash Mahala <avinash.mahala.am@outlook.com>
2023-09-06 01:41:24 -0500

    Implemented Sample Splash Screen

--- app/src/main/java/com/codecrafters/quizquest/activities/RegistrationActivity.java
+++ app/src/main/java/com/codecrafters/quizquest/activities/RegistrationActivity.java
@@ -1,16 +1,20 @@
 package com.codecrafters.quizquest.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
-
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import com.codecrafters.quizquest.R;
 
 public class RegistrationActivity extends AppCompatActivity {
+    // Implement user registration logic here
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        // Handle user registration when they sign up
+        // Redirect to the appropriate dashboard based on user role
     }
 }
--- app/src/main/java/com/codecrafters/quizquest/activities/SplashScreenActivity.java
+++ app/src/main/java/com/codecrafters/quizquest/activities/SplashScreenActivity.java
@@ -1,16 +1,31 @@
 package com.codecrafters.quizquest.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.os.Handler;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import com.codecrafters.quizquest.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+    // Set the duration of the splash screen in milliseconds (e.g., 5000ms = 5 seconds)
+    private static final int SPLASH_DURATION = 5000;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        // Use a Handler to delay the navigation to the RegistrationActivity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Intent to navigate to the RegistrationActivity
+                Intent intent = new Intent(SplashScreenActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+                finish(); // Close the splash screen activity
+            }
+        }, SPLASH_DURATION);
     }
 }
