5dac679bb19cb7db694ef3cd8af3caa47a333b3e
Avinash Mahala <avinash.mahala.am@outlook.com>
2023-09-06 02:28:26 -0500

    Registration and Login Skeleton Done

--- app/src/main/java/com/codecrafters/quizquest/activities/SplashScreenActivity.java
+++ app/src/main/java/com/codecrafters/quizquest/activities/SplashScreenActivity.java
@@ -10,19 +10,19 @@ import com.codecrafters.quizquest.R;
 public class SplashScreenActivity extends AppCompatActivity {
 
     // Set the duration of the splash screen in milliseconds (e.g., 5000ms = 5 seconds)
-    private static final int SPLASH_DURATION = 5000;
+    private static final int SPLASH_DURATION = 2000;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        // Use a Handler to delay the navigation to the RegistrationActivity
+        // Use a Handler to delay the navigation to the CommonLoginRegistrationActivity
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                // Intent to navigate to the RegistrationActivity
-                Intent intent = new Intent(SplashScreenActivity.this, RegistrationActivity.class);
+                // Intent to navigate to the CommonLoginRegistrationActivity
+                Intent intent = new Intent(SplashScreenActivity.this, CommonLoginRegistrationActivity.class);
                 startActivity(intent);
                 finish(); // Close the splash screen activity
             }
