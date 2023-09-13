aa98566eac5541390849a48b7717e3da6fd82a0d
yashj411 <70331087+yashj411@users.noreply.github.com>
2023-09-12 20:52:46 -0500

    Implemented Firebase connection for Login and Registration

--- app/src/main/java/com/codecrafters/quizquest/activities/CommonLoginRegistrationActivity.java
+++ app/src/main/java/com/codecrafters/quizquest/activities/CommonLoginRegistrationActivity.java
@@ -3,29 +3,41 @@ package com.codecrafters.quizquest.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.codecrafters.quizquest.MainActivity;
 import com.codecrafters.quizquest.R;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.AuthResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
 
 public class CommonLoginRegistrationActivity extends AppCompatActivity {
 
     private EditText emailEditText;
     private EditText passwordEditText;
-    private Spinner roleSpinner;
     private Button registerButton;
     private Button loginButton;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_common_login_registration);
+        // taking instance of FirebaseAuth
+        mAuth = FirebaseAuth.getInstance();
 
         // Initialize views
         emailEditText = findViewById(R.id.emailEditText);
         passwordEditText = findViewById(R.id.passwordEditText);
-        roleSpinner = findViewById(R.id.roleSpinner);
         registerButton = findViewById(R.id.registerButton);
         loginButton = findViewById(R.id.loginButton);
 
@@ -51,23 +63,76 @@ public class CommonLoginRegistrationActivity extends AppCompatActivity {
 //                String email = emailEditText.getText().toString();
 //                String password = passwordEditText.getText().toString();
 //                String selectedRole = roleSpinner.getSelectedItem().toString();
-
-                String selectedRole="User";//Sample As of Now
-
-                // Implement your login logic here
-
-                // After successful login, you can navigate to the appropriate dashboard or user profile page
-                // For example:
-                if (selectedRole.equals("Admin")) {
-                    // Navigate to the Admin Dashboard
-                    Intent adminDashboardIntent = new Intent(CommonLoginRegistrationActivity.this, AdminDashboardActivity.class);
-                    startActivity(adminDashboardIntent);
-                } else if (selectedRole.equals("User")) {
-                    // Navigate to the User Dashboard
-                    Intent userDashboardIntent = new Intent(CommonLoginRegistrationActivity.this, UserDashboardActivity.class);
-                    startActivity(userDashboardIntent);
-                }
+                loginUserAccount();
+//                String selectedRole="User";//Sample As of Now
+//
+//                // Implement your login logic here
+//
+//                // After successful login, you can navigate to the appropriate dashboard or user profile page
+//                // For example:
+//                if (selectedRole.equals("Admin")) {
+//                    // Navigate to the Admin Dashboard
+//                    Intent adminDashboardIntent = new Intent(CommonLoginRegistrationActivity.this, AdminDashboardActivity.class);
+//                    startActivity(adminDashboardIntent);
+//                } else if (selectedRole.equals("User")) {
+//                    // Navigate to the User Dashboard
+//                    Intent userDashboardIntent = new Intent(CommonLoginRegistrationActivity.this, UserDashboardActivity.class);
+//                    startActivity(userDashboardIntent);
+//                }
             }
         });
+    }
+
+    private void loginUserAccount() {
+        String email, password;
+        email = emailEditText.getText().toString();
+        password = passwordEditText.getText().toString();
+
+        // validations for input email and password
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(),
+                            "Please enter email!!",
+                            Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(),
+                            "Please enter password!!",
+                            Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+
+        // signin existing user
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(
+                        task -> {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(getApplicationContext(),
+                                                "Login successful!!",
+                                                Toast.LENGTH_LONG)
+                                        .show();
+
+
+                                // if sign-in is successful
+                                // intent to home activity
+                                Intent intent
+                                        = new Intent(CommonLoginRegistrationActivity.this,
+                                        QuizCategoriesActivity.class);
+                                startActivity(intent);
+                            }
+
+                            else {
+
+                                // sign-in failed
+                                Toast.makeText(getApplicationContext(),
+                                                "Wrong Email or password!!",
+                                                Toast.LENGTH_LONG)
+                                        .show();
+
+                            }
+                        });
     }
 }
--- app/src/main/java/com/codecrafters/quizquest/activities/RegistrationActivity.java
+++ app/src/main/java/com/codecrafters/quizquest/activities/RegistrationActivity.java
@@ -2,12 +2,31 @@ package com.codecrafters.quizquest.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.ProgressBar;
+
 import com.codecrafters.quizquest.R;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.AuthResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+
 public class RegistrationActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText emailTextView, passwordTextView, nameTextView, phonenumberTextView, dobTextView;
+    private Button Btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +35,21 @@ public class RegistrationActivity extends AppCompatActivity {
 
         // Find the "Back to Common Login" button by its ID
         Button backToCommonButton = findViewById(R.id.backToCommonButton);
+        // taking FirebaseAuth instance
+        mAuth = FirebaseAuth.getInstance();
+
+        emailTextView = findViewById(R.id.emailEditText);
+        passwordTextView = findViewById(R.id.passwordEditText);
+        String uid = mAuth.getCurrentUser().getUid();
+        Btn = findViewById(R.id.registerButton);
+
+        Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                registerNewUser();
+            }
+        });
 
         // Set a click listener for the button
         backToCommonButton.setOnClickListener(new View.OnClickListener() {
@@ -28,4 +62,69 @@ public class RegistrationActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void registerNewUser() {
+        // Take the value of two edit texts in Strings
+        String email, password;
+        email = emailTextView.getText().toString();
+        password = passwordTextView.getText().toString();
+
+        // Validations for input email and password
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(),
+                            "Please enter email!!",
+                            Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(),
+                            "Please enter password!!",
+                            Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+
+        // Create a new user or register a new user
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(),
+                                            "Registration successful!",
+                                            Toast.LENGTH_LONG)
+                                    .show();
+
+                            // If the user is created, navigate to the login activity
+                            Intent intent = new Intent(RegistrationActivity.this, CommonLoginRegistrationActivity.class);
+                            startActivity(intent);
+                        } else {
+                            // Registration failed, handle exceptions
+                            try {
+                                throw task.getException();
+                            } catch (FirebaseAuthUserCollisionException e) {
+                                // Handle case where email is already in use
+                                Toast.makeText(getApplicationContext(),
+                                                "Email is already in use!",
+                                                Toast.LENGTH_LONG)
+                                        .show();
+                            } catch (FirebaseAuthInvalidCredentialsException e) {
+                                // Handle invalid email format
+                                Toast.makeText(getApplicationContext(),
+                                                "Invalid email or password format!",
+                                                Toast.LENGTH_LONG)
+                                        .show();
+                            } catch (Exception e) {
+                                // Handle other exceptions
+                                Toast.makeText(getApplicationContext(),
+                                                "Registration failed: " + e.getMessage(),
+                                                Toast.LENGTH_LONG)
+                                        .show();
+                            }
+                        }
+                    }
+                });
+    }
+
 }
