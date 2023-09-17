e54e83f4cbc3da20809aef0595d20c4237c1cba2
nehaparbate <parbate.neha20@gmail.com>
2023-09-17 13:32:51 -0500

    Quiz Categories Page Static Data & Database init
    Created Quiz Categories Page and relevant components. Although Data for being now is static. Initiated DB Connection.

--- app/src/main/java/com/codecrafters/quizquest/activities/QuizCategoriesActivity.java
+++ app/src/main/java/com/codecrafters/quizquest/activities/QuizCategoriesActivity.java
@@ -1,16 +1,44 @@
 package com.codecrafters.quizquest.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import de.hdodenhof.circleimageview.CircleImageView;
 import android.os.Bundle;
 
 import com.codecrafters.quizquest.R;
+import com.codecrafters.quizquest.adapters.QuizCategoryAdapter;
+import com.codecrafters.quizquest.models.QuizCategory;
+
+import java.util.ArrayList;
 
 public class QuizCategoriesActivity extends AppCompatActivity {
+
+    ArrayList<QuizCategory> arrCategory = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_quiz_categories);
+        setContentView(R.layout.activity_quiz_grid);
+        RecyclerView rc = findViewById(R.id.recycleList);
+        rc.setLayoutManager(new GridLayoutManager(this, 2));
+
+        arrCategory.add( new QuizCategory("Astronomy",R.drawable.astronomy,"1"));
+        arrCategory.add( new QuizCategory("Chemistry",R.drawable.chemistry,"2"));
+        arrCategory.add( new QuizCategory("Geography",R.drawable.geography,"3"));
+        arrCategory.add( new QuizCategory("History",R.drawable.history,"4"));
+        arrCategory.add( new QuizCategory("Physics",R.drawable.physics,"5"));
+        arrCategory.add( new QuizCategory("Sports",R.drawable.sports,"6"));
+        arrCategory.add( new QuizCategory("Chemistry",R.drawable.chemistry,"2"));
+        arrCategory.add( new QuizCategory("Geography",R.drawable.geography,"3"));
+        arrCategory.add( new QuizCategory("History",R.drawable.history,"4"));
+        arrCategory.add( new QuizCategory("Physics",R.drawable.physics,"5"));
+        arrCategory.add( new QuizCategory("Sports",R.drawable.sports,"6"));
+        QuizCategoryAdapter adapter = new QuizCategoryAdapter(this, arrCategory);
+        rc.setAdapter(adapter);
+
+
     }
 }
--- app/src/main/java/com/codecrafters/quizquest/models/QuizCategory.java
+++ app/src/main/java/com/codecrafters/quizquest/models/QuizCategory.java
@@ -1,4 +1,38 @@
 package com.codecrafters.quizquest.models;
 
 public class QuizCategory {
+
+    private String quizCatName;
+    private int quizCatImg;
+    private String catKey;
+
+    public QuizCategory(String quizCatName, int quizCatImg, String catKey) {
+        this.quizCatName = quizCatName;
+        this.quizCatImg = quizCatImg;
+        this.catKey = catKey;
+    }
+
+    public String getQuizCatName() {
+        return quizCatName;
+    }
+
+    public void setQuizCatName(String quizCatName) {
+        this.quizCatName = quizCatName;
+    }
+
+    public int getQuizCatImg() {
+        return quizCatImg;
+    }
+
+    public void setQuizCatImg(int quizCatImg) {
+        this.quizCatImg = quizCatImg;
+    }
+
+    public String getCatKey() {
+        return catKey;
+    }
+
+    public void setCatKey(String catKey) {
+        this.catKey = catKey;
+    }
 }
