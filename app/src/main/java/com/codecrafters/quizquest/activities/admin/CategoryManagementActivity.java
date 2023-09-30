e2bbe5403e4f239d2ae18d4587391ed956d1befe
Avinash Mahala <avinash.mahala.am@outlook.com>
2023-09-30 16:43:56 -0500

    Added New Activities and Buttons

---
+++ app/src/main/java/com/codecrafters/quizquest/activities/admin/CategoryManagementActivity.java
@@ -0,0 +1,16 @@
+package com.codecrafters.quizquest.activities.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.codecrafters.quizquest.R;
+
+public class CategoryManagementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_management);
+    }
+}
---
+++ app/src/main/java/com/codecrafters/quizquest/activities/admin/QuestionManagementActivity.java
@@ -0,0 +1,16 @@
+package com.codecrafters.quizquest.activities.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.codecrafters.quizquest.R;
+
+public class QuestionManagementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question_management);
+    }
+}
