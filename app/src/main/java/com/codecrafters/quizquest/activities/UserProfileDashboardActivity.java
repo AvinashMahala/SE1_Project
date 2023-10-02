bf948aace3ff4ffe30946abbda7f8aac73165edb
Rohit Jain <rohit@Rohits-MacBook-Air.local>
2023-10-01 22:14:17 -0500

    updated user dashboard xml

--- app/src/main/java/com/codecrafters/quizquest/activities/UserProfileDashboardActivity.java
+++ app/src/main/java/com/codecrafters/quizquest/activities/UserProfileDashboardActivity.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 import com.codecrafters.quizquest.R;
 import com.codecrafters.quizquest.adapters.QuizHistoryAdapter;
 import com.codecrafters.quizquest.viewmodels.UserProfileDashboardViewModel;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
 
@@ -20,6 +21,7 @@ public class UserProfileDashboardActivity extends AppCompatActivity {
 
     private UserProfileDashboardViewModel viewModel;
     private String userId;
+    private FirebaseAuth mAuth; // Firebase Authentication instance
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,17 +29,22 @@ public class UserProfileDashboardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user_profile_dashboard);
 
         viewModel = new ViewModelProvider(this).get(UserProfileDashboardViewModel.class);
+        mAuth = FirebaseAuth.getInstance(); // Initialize Firebase Authentication
 
         userId = getIntent().getStringExtra("USER_ID");
 
+        // Load user data and quiz history based on the received user ID
         viewModel.loadUserData(userId);
         viewModel.loadQuizHistoryData(userId);
 
+        // Find and initialize the TextViews and ListView
         TextView userNameTextView = findViewById(R.id.userName);
         TextView userQuizCountTextView = findViewById(R.id.userQuizCount);
         ListView quizHistoryListView = findViewById(R.id.quizHistoryList);
         TextView noQuizMessageTextView = findViewById(R.id.noQuizMessage);
         Button startNewQuizButton = findViewById(R.id.startNewQuizButton);
+        Button logoutButton = findViewById(R.id.logoutButton); // Logout button
+        Button userProfileButton = findViewById(R.id.userProfileButton); // User profile button
 
         viewModel.getUser().observe(this, user -> {
             userNameTextView.setText("Welcome " + user.getName());
@@ -54,18 +61,41 @@ public class UserProfileDashboardActivity extends AppCompatActivity {
                 noQuizMessageTextView.setVisibility(View.GONE);
                 quizHistoryListView.setVisibility(View.VISIBLE);
 
+                // Create an adapter to display the quiz history in the ListView
                 QuizHistoryAdapter adapter = new QuizHistoryAdapter(this, new ArrayList<>(quizHistory));
                 quizHistoryListView.setAdapter(adapter);
 
+                // Display the quiz count
                 String quizCountText = "Quizzes taken: " + quizCount;
                 userQuizCountTextView.setText(quizCountText);
             }
         });
 
+        // Set OnClickListener for the "Start New Quiz" button
         startNewQuizButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(UserProfileDashboardActivity.this, QuizCategoriesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Set OnClickListener for the "Logout" button
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut(); // Sign out from Firebase Authentication
+                Intent intent = new Intent(UserProfileDashboardActivity.this, CommonLoginRegistrationActivity.class);
+                startActivity(intent);
+                finish(); // Close the current activity
+            }
+        });
+
+        // Set OnClickListener for the "User Profile" button
+        userProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(UserProfileDashboardActivity.this, UserProfileActivity.class);
                 startActivity(intent);
             }
         });
