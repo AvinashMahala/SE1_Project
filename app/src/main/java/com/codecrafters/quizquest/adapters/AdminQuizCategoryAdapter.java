c361660068223726ef6418b89236978a5684439c
Avinash Mahala <avinash.mahala.am@outlook.com>
2023-10-03 08:21:53 -0500

    update

--- app/src/main/java/com/codecrafters/quizquest/adapters/AdminQuizCategoryAdapter.java
+++ app/src/main/java/com/codecrafters/quizquest/adapters/AdminQuizCategoryAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,6 +12,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.codecrafters.quizquest.models.AdminQuizCategory;
 import com.codecrafters.quizquest.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import android.widget.Toast;
 
 import java.util.List;
@@ -56,6 +62,7 @@ public class AdminQuizCategoryAdapter extends RecyclerView.Adapter<AdminQuizCate
                     // Handle category item click
                 }
             });
+
         } catch (Exception e) {
             e.printStackTrace();
 //            showToast("Error loading category data: " + e.getMessage());
@@ -74,9 +81,30 @@ public class AdminQuizCategoryAdapter extends RecyclerView.Adapter<AdminQuizCate
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             categoryName = itemView.findViewById(R.id.adminCategoryName);
-            categoryDescription = itemView.findViewById(R.id.adminCategoryDesc);
+            //categoryDescription = itemView.findViewById(R.id.adminCategoryDesc);
         }
     }
+
+    private void deleteQuizCategory(AdminQuizCategory category) {
+        DatabaseReference categoryRef = FirebaseDatabase.getInstance().getReference().child("QuizCategories").child(category.getQuizCatID());
+        categoryRef.removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                // Successfully deleted
+                Toast.makeText(context, "Quiz category deleted successfully!", Toast.LENGTH_SHORT).show();
+                // Remove the category from the local list and notify the adapter
+                adminQuizCategories.remove(category);
+                notifyDataSetChanged();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                // Failed to delete
+                Toast.makeText(context, "Error deleting quiz category: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
 
 
 
