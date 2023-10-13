4da943e17e8e8f32a87e0f4cd4ca65e1e65452a6
Avinash Mahala <avinash.mahala.am@outlook.com>
2023-10-13 03:24:46 -0500

    Added QuizSetActivity

---
+++ app/src/main/java/com/codecrafters/quizquest/activities/admin/QuizSetActivity.java
@@ -0,0 +1,16 @@
+package com.codecrafters.quizquest.activities.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.codecrafters.quizquest.R;
+
+public class QuizSetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_set);
+    }
+}
